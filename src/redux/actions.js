5de96da72d501ea7d777a73275b12f8f9bc25bfe
Liudmyla_Taran <taran.lyudok@gmail.com>
2025-11-30 12:47:44 +0200

    remove file

--- src/redux/actions.js
+++
@@ -1,6 +0,0 @@
-import { createAction } from "@reduxjs/toolkit";
-
-export const actionAddToDo = createAction("ToDo/addToDo");
-
-export const actionDeleteToDo = createAction("ToDo/deleteToDo");
-export const actionToggleCompleted = createAction("ToDo/toggleCompleted");
